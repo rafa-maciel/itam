5d23013b6d08fe08af56b4ce5fd2d8103e9dc17b
Rafa Maciel <rafael.macielf@gmail.com>
2021-02-24 16:20:44 -0300

    created search to model that contains parameter

--- src/main/java/com/rmaciel/itam/itam/deviceModels/DeviceModelRepository.java
+++ src/main/java/com/rmaciel/itam/itam/deviceModels/DeviceModelRepository.java
@@ -1,7 +1,13 @@
 package com.rmaciel.itam.itam.deviceModels;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface DeviceModelRepository extends CrudRepository<DeviceModel, Long>{
+
+    @RestResource(path = "modelContains", rel = "modelContains")
+    Page<DeviceModel> findByModelContains(String model, Pageable p);
     
 }
