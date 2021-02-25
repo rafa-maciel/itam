c950962e25ff3c2d073bbc402f997ed7edffe23d
Rafa Maciel <rafael.macielf@gmail.com>
2021-02-25 12:05:51 -0300

    created endpoint and repository path to find by title or city

--- src/main/java/com/rmaciel/itam/itam/Locations/LocationRepository.java
+++ src/main/java/com/rmaciel/itam/itam/Locations/LocationRepository.java
@@ -1,7 +1,12 @@
 package com.rmaciel.itam.itam.Locations;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 public interface LocationRepository extends CrudRepository<Location, Long>{
     
+    @RestResource(path = "locationContains", rel = "locationContains")
+    Page<Location> findByTitleContainsOrCityContains(String title, String city, Pageable p);
 }
