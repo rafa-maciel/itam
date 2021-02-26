7700e54b54d42b2a66071124e4fb91145322403d
Rafa Maciel <rafael.macielf@gmail.com>
2021-02-26 09:28:07 -0300

    removed unused code (extra api code to get relational objects are not necessary anymore)

--- src/main/js/components/assets/AssetUpdate/AssetUpdate.jsx
+++ src/main/js/components/assets/AssetUpdate/AssetUpdate.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
-import { apiNav, apiNavMany, apiUpdate } from '../../../api/api';
+import { apiNav, apiUpdate } from '../../../api/api';
 import { AssetForm } from '../';
 
 export default function AssetUpdate() {
@@ -13,32 +13,6 @@ export default function AssetUpdate() {
         if (assetURL) {
             apiNav(assetURL)
                 .then(asset => {
-                    let relurls = Object.keys(asset._links)
-                        .filter(key => key != 'self' && key != 'asset')
-                        .map(key => asset._links[key].href)
-
-                    apiNavMany(relurls)
-                    .then(responses => {
-                        responses.forEach(res => {
-                            switch (res.config.url) {
-                                case asset._links.owner.href:
-                                    asset.owner = res.data
-                                    break;
-
-                                case asset._links.location.href:
-                                    asset.location = res.data
-                                    break;
-
-                                case asset._links.model.href:
-                                    asset.model = res.data
-                                    break;
-                            
-                                default:
-                                    break;
-                            }
-                        })
-                    })
-
                     setAssetData(asset)
                 })
         }
