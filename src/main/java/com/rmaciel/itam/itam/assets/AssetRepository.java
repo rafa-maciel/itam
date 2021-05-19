2e146df36c0a3153d2fa6e13510daa1faecbf335
Rafa Maciel <rafael.macielf@gmail.com>
2021-05-19 10:37:55 -0300

    created query for find assets by owner

--- src/main/java/com/rmaciel/itam/itam/assets/AssetRepository.java
+++ src/main/java/com/rmaciel/itam/itam/assets/AssetRepository.java
@@ -1,11 +1,17 @@
 package com.rmaciel.itam.itam.assets;
 
+import java.util.List;
+
+import com.rmaciel.itam.itam.Users.User;
 import com.rmaciel.itam.itam.assets.projections.AssetProjection;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(excerptProjection = AssetProjection.class)
 public interface AssetRepository extends PagingAndSortingRepository<Asset, Long> {
     
+    @RestResource(path = "allFromOwner", rel = "allFromOwner")
+    List<Asset> findByOwnerId(Long ownerId);
 }
